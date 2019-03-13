563b0fc5f44e2277e1c75186f3a23cf0959c6383
tae-lim <taelim92@gmail.com>
2019-03-12 18:31:30 -0700

    Implement bodyparser dependency

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, '../client')));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Listening on Port: ${port}`);
